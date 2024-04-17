de0e8771031de4457ca8836a564c670b20d31248
SEONG YUN JEONG <50621327+jeoungsung12@users.noreply.github.com>
2024-04-17 17:18:22 +0900

    [level 1] Title: 폰켓몬, Time: 0.23 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,13 +1,12 @@
-def solution(nums):
-    answer = 0
-    k = set(nums)
-    k = list(k)
-    t1 = len(k) #종류의 수
-    t2 = len(nums)//2 #가질 수
-    if(t1 > t2):
-        answer = t2
-    elif(t1 == t2):
-        answer = t1
+def solution(n):
+    s = set(n) #집합 중복처리
+    c1 = len(s) #종류의 수
+    c2 = len(n) #전체의 수
+    N = c2 // 2 #뽑아야 할 수
+    if N < c1:
+        answer = N
+    elif N > c1:
+        answer = c1
     else:
-        answer = t1
+        answer = c1
     return answer
