128de72b1dc604e2b395a941c1deba52c07696f0
SEONG YUN JEONG <50621327+jeoungsung12@users.noreply.github.com>
2024-04-18 14:34:38 +0900

    [level 2] Title: 올바른 괄호, Time: 8.68 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,22 +1,9 @@
-def solution(s):
-    answer = True
-    stack = [None for _ in range(len(s))]
-    s = list(s)
-    end = 0
-    if(s[0] == '(') & (len(s) != 1):
-        for i in s:
-            if(i == '('):
-                stack[end] = i
-                end += 1
-            elif(i == ')'):
-                if(stack[end-1] == '('):
-                    stack[end-1] = None
-                    end -= 1
-        stack = set(stack)
-        if(len(stack) == 1):
-            answer = True
-        else:
-            answer = False
-    else:
-        answer = False
-    return answer
+def solution(S):
+    stack = []
+    for s in S:
+        if s == "(":
+            stack.append(s)
+        elif s == ")":
+            if not stack or stack.pop() != "(":
+                return False
+    return not stack
