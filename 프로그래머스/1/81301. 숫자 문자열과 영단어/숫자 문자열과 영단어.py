8636aa904681cd54997bbd2390ca135a1d838811
SEONG YUN JEONG <50621327+jeoungsung12@users.noreply.github.com>
2024-05-07 15:29:22 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,36 +1,15 @@
 def solution(s):
-    answer = 0
-    l = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-    t = ''
-    a = ''
-    for i in range(len(s)):
-        if(s[i] >= '0') & (s[i] <= '9'):
-            a += s[i]
-        elif(s[i] >= 'a') & (s[i] <= 'z'):
-            t += s[i]
-        for j in range(len(l)):
-            if(t == l[j]):
-                if(t == 'zero'):
-                    a += '0'
-                elif(t == 'one'):
-                    a += '1'
-                elif(t == 'two'):
-                    a += '2'
-                elif(t == 'three'):
-                    a += '3'
-                elif(t == 'four'):
-                    a += '4'
-                elif(t == 'five'):
-                    a += '5'
-                elif(t == 'six'):
-                    a += '6'
-                elif(t == 'seven'):
-                    a += '7'
-                elif(t == 'eight'):
-                    a += '8'
-                elif(t == 'nine'):
-                    a += '9'
-                t = ''
+    stack = ""
+    answer = ""
+    L = [["zero","0"],["one","1"],["two","2"],["three","3"],["four","4"],["five","5"],["six","6"],["seven","7"],["eight","8"],["nine","9"]]
+    for x in s:
+        if (x >= "0") & (x <= "9"):
+            answer += x
+        else:
+            stack += x
+            for l in L:
+                if l[0] == stack:
+                    stack = ""
+                    answer += l[1]
             
-    answer = int(a)
-    return answer
+    return int(answer)
