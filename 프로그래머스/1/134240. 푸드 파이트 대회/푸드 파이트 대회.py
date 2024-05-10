8132ff99d53eb63b7c8fe9166ac6accd7b2f6240
SEONG YUN JEONG <50621327+jeoungsung12@users.noreply.github.com>
2024-05-10 16:22:02 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,19 +1,19 @@
-def solution(food):
-    answer = ''
-    count = 1
-    a = ''
-    b = ''
-    k = [1,2,3,4,5,6,7,8,9]
-    for i in range(1,len(food)):
-        if(food[i] % 2 >= 0):
-            count += ((food[i] // 2) * 2)
-    for i in range(1, len(food)):
-        if(food[i] % 2 >= 0):
-            for j in range(0,food[i] // 2, 1):
-                a += str(k[i-1])
-            for j in range(food[len(food)-i] // 2, 0, -1):
-                b += str(k[len(food)-i-1])
-    answer += a
-    answer += '0'
-    answer += b
+def solution(F):
+    answer = ""
+    index = []
+    for i in range(len(F)):
+        if F[i] < 2:
+            continue
+        else:
+            index.append([i, (F[i] // 2) * 2])
+    for i in index:
+        num = i[1]
+        for _ in range(num // 2):
+            answer += str(i[0])
+        
+    re = answer
+    re = reversed(re)
+    re = "".join(re)
+    answer += "0"
+    answer += re
     return answer
