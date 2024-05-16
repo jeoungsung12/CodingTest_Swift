d0068d1e1740bed8df77f6b7271cf4f48c3c0f48
SEONG YUN JEONG <50621327+jeoungsung12@users.noreply.github.com>
2024-05-16 14:58:12 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -1,17 +1,14 @@
 def solution(nums):
     answer = 0
-    l = []
-    p = 0
+    result = 0
     for i in range(len(nums)):
-        for j in range(i+1,len(nums)):
-            for k in range(j+1,len(nums)):
-                #print((nums[i] + nums[j] + nums[k]))
-                for t in range(1,(nums[i] + nums[j] + nums[k])+1):
-                    if((nums[i] + nums[j] + nums[k]) % t == 0):
-                        answer += 1
-                if(answer == 2):
-                    p += 1
+        for j in range(i+1, len(nums)):
+            for k in range(j+1, len(nums)):
+                for t in range(1, (nums[i] + nums[j] + nums[k] + 1)):
+                    if (nums[i]+nums[j]+nums[k]) % t == 0:
+                        answer +=1
+                if answer == 2:
+                    result += 1
                 answer = 0
-    answer = p
-        
+    answer = result
     return answer
