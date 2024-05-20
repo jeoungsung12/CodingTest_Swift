1eae1515e3334a880f15fae000d299836a2fe602
SEONG YUN JEONG <50621327+jeoungsung12@users.noreply.github.com>
2024-05-20 16:14:43 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,28 +1,32 @@
-def solution(N, stages):
-    answer = []
+def solution(N,stages):
+    #실패율 : 스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수 / 스테이지에 도달한 플레이어
+    #전체 스테이지 개수 N
+    #사용자가 현재 멈춰있는 스테이지 번호 stages
+    # 스테이지 별로 실패율을 저장할 리스트
+    fail_rates = []
     
-    # 스테이지별 플레이어 수를 저장하는 리스트 생성 및 초기화
-    player_counts = [0] * (N+2)
+    # 총 플레이어 수
+    total_players = len(stages)
     
-    # 각 스테이지별 플레이어 수 계산
-    for stage in stages:
-        player_counts[stage] += 1
-    
-    # 스테이지별 실패율과 스테이지 번호를 저장하는 리스트 생성
-    failure_rates = []
-    for i in range(1, N+1):
-        total_players = sum(player_counts[i:]) # 이전 스테이지까지의 플레이어 수 누적
-        if total_players == 0:
-            failure_rate = 0
+    for stage in range(1, N + 1):
+        # 현재 스테이지에 도달했으나 아직 클리어하지 못한 플레이어 수
+        not_cleared = stages.count(stage)
+        
+        # 실패율 계산
+        if total_players > 0:
+            fail_rate = not_cleared / total_players
         else:
-            failure_rate = player_counts[i] / total_players
-        failure_rates.append((failure_rate, i))
+            fail_rate = 0
+        
+        # 스테이지 번호와 실패율을 리스트에 추가
+        fail_rates.append((stage, fail_rate))
+        
+        # 다음 스테이지 계산을 위해 총 플레이어 수를 업데이트
+        total_players -= not_cleared
     
-    # 실패율을 기준으로 리스트 정렬
-    failure_rates.sort(key=lambda x: (-x[0], x[1]))
+    # 실패율을 기준으로 내림차순, 실패율이 같으면 스테이지 번호로 오름차순 정렬
+    fail_rates.sort(key=lambda x: (-x[1], x[0]))
     
-    # 정렬된 스테이지 번호를 answer 리스트에 추가
-    for failure_rate, stage_number in failure_rates:
-        answer.append(stage_number)
-    
+    # 정렬된 실패율 리스트에서 스테이지 번호만 추출하여 반환
+    answer = [stage for stage, _ in fail_rates]
     return answer
