72a05f88a4f208714366ef2b35129e03f71e3417
SEONG YUN JEONG <50621327+jeoungsung12@users.noreply.github.com>
2025-03-18 12:19:34 +0900

    [Bronze V] Title: 사칙연산, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.swift
@@ -0,0 +1,10 @@
+let line = readLine()!
+let lineArray = line.split(separator: " ")
+if let a = Int(lineArray[0]),
+   let b = Int(lineArray[1]) {
+    print(a + b)
+    print(a - b)
+    print(a * b)
+    print(a / b)
+    print(a % b)
+}
